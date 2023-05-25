5074266b72bed79f0fe613b62ea10bb7ae8800cd
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-05-25 18:28:52 -0300

    Update app.py
    Terceiro

--- app.py
+++ app.py
@@ -8,10 +8,4 @@ stocks = investpy.get_stocks_list(country='brazil')
 # Criar um DataFrame pandas com os tickets das ações
 df = pd.DataFrame({'Ticket': stocks})
 
-# Exibir a tabela no Streamlit
-def app():
-    st.table(df)
-
-# Executar o aplicativo Streamlit
-if __name__ == '__main__':
-    app()
+st.table(df)
