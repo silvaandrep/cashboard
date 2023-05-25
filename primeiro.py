36615296aa55cb344893968f08a569b79e6c7fe2
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-05-25 19:34:47 -0300

    Create primeiro.py
    c

---
+++ primeiro.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+# Título do aplicativo
+st.title('Meu Primeiro Aplicativo Streamlit')
+
+# Adicionando um texto
+st.write('Bem-vindo ao meu aplicativo!')
+
+# Adicionando um gráfico
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.DataFrame(np.random.randn(100, 2), columns=['x', 'y'])
+st.line_chart(data)
+
+# Adicionando um controle de entrada
+user_input = st.text_input("Digite seu nome:")
+st.write('Olá, ', user_input, '!')
+
+# Exibindo uma tabela
+st.write(data)
