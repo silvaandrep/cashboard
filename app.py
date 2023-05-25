cbc81faa0212487b2bf91f9bd94a30cbc2d5d230
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-05-25 18:27:32 -0300

    Update app.py
    Segundo commit

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+import investpy
 
-# Título do aplicativo
-st.title('Meu Primeiro Aplicativo Streamlit')
+# Obter lista de ações brasileiras
+stocks = investpy.get_stocks_list(country='brazil')
 
-# Adicionando um texto
-st.write('Bem-vindo ao meu aplicativo!')
+# Criar um DataFrame pandas com os tickets das ações
+df = pd.DataFrame({'Ticket': stocks})
+
+# Exibir a tabela no Streamlit
+def app():
+    st.table(df)
+
+# Executar o aplicativo Streamlit
+if __name__ == '__main__':
+    app()
