bbbb61cf320a19db0069798c468f25bb7c014e41
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-05-26 20:12:54 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import streamlit as st
 import investpy
+import yfinance as yf
+import plotly.graph_objects as go
+import streamlit as st
 
 # Obter lista de ações brasileiras
 stocks = investpy.get_stocks_list(country='brazil')
@@ -7,21 +9,43 @@ stocks = investpy.get_stocks_list(country='brazil')
 # Ordenar a lista de tickets em ordem alfabética
 sorted_stocks = sorted(stocks)
 
-# Dividir a tela em 1/3 e 2/3
-col1, col2 = st.columns([1, 2])
 
-# Adicionar elementos à primeira coluna
-with col1:
-    st.header('Coluna 1')
-    st.write('Conteúdo da coluna 1')
-    
 # Criar o combobox no Streamlit
-selected_option = st.selectbox('Selecione uma opção', stocks)
+#selected_option = st.selectbox('Selecione uma opção', stocks)
 
 # Exibir a opção selecionada
-st.write('Você selecionou:', selected_option)
+#st.write('Você selecionou:', selected_option)
 
-# Adicionar elementos à segunda coluna
-with col2:
-    st.header('Coluna 2')
-    st.write('Conteúdo da coluna 2')
+# Configurar o título do aplicativo
+st.title('Gráfico de Velas - PETR3.SA')
+
+
+# Configurar a barra lateral
+st.sidebar.header('Coluna 1')
+
+
+#valor = st.sidebar.slider('Selecione um valor', 0, 100, 50)
+opcao = st.sidebar.selectbox('Selecione uma ação', stocks)
+st.sidebar.write('Você selecionou ', opcao)
+
+# Adicionar elementos à coluna principal
+st.header('Coluna 2')
+st.write(opcao)
+
+opcao = opcao + ".SA"
+
+# Obter os dados da ação PETR3.SA usando a biblioteca yfinance
+data = yf.download(opcao, start='2022-01-01', end='2022-12-31')
+
+# Criar o gráfico de velas usando o Plotly
+fig = go.Figure(data=[go.Candlestick(x=data.index,
+                                     open=data['Open'],
+                                     high=data['High'],
+                                     low=data['Low'],
+                                     close=data['Close'])])
+
+# Configurar o layout do gráfico
+fig.update_layout(xaxis_rangeslider_visible=False)
+
+# Exibir o gráfico no Streamlit
+st.plotly_chart(fig)
