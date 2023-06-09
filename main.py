26948babad413a02630346f148b28d3f1e531852
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-06-09 12:21:21 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import investpy as ip
 import streamlit as st
+
+opcoes = ['Opção 1', 'Opção 2', 'Opção 3']
+
+# Criar o combobox no Streamlit
+selected_option = st.selectbox('Selecione uma opção', opcoes)
 
 st.title('Title Principal')
 st.header('Header Principal')
-st.write('Write Principal')
+st.write('Você selecionou:', selected_option)
 st.sidebar.header('Header Sidebar')
 st.sidebar.write('Write Sidebar')
+
