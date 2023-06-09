5652a18906fc2dfe08d4a47460994f2b0501bc2f
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-06-09 17:03:00 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-#import investpy
+import investpy
+import yfinance as yf
+import plotly.graph_objects as go
 import streamlit as st
 
 tickers = investpy.get_stocks_list(country='brazil')
 tickers = sorted(tickers)
 
-#st.sidebar.header('Menu de Ações da B3')
+st.sidebar.header('MENU DE AÇÕES DA B3')
 
-#ticker = st.sidebar.selectbox('Selecione uma ação: ', tickers)
+ticker = st.sidebar.selectbox('Selecione uma ação: ', tickers)
 
-#st.sidebar.write('Você selecionou ', ticker)
+ticker = ticker + ".SA"
 
-#st.header(ticker)
-#st.write('Você selecionou ', ticker)
+st.sidebar.write('Total de ações da B3: ', len(tickers))
+
+
+st.header(ticker)
+st.write('Você selecionou ', ticker)
+
+períodos = ['5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'max']
+
+periodo = st.sidebar.selectbox('Selecione o período: ', períodos)
+
+dados = yf.download(ticker, period=periodo)
+
+fig = go.Figure(data=go.Scatter(x=dados.index, y=dados['Close']))
+fig.update_layout(title=f"Gráfico de Preço de Fechamento para {ticker}")
+st.plotly_chart(fig)
