654f9cc070e215d585e75e9439b1421932f9ec77
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-06-09 13:08:32 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ st.write('Você selecionou')
 st.sidebar.header('Header Sidebar')
 st.sidebar.write('Write Sidebar')
 
+# Lista de opções para o combobox
+opcoes = ['Opção 1', 'Opção 2', 'Opção 3']
+
+# Criar o combobox no Streamlit
+selected_option = st.selectbox('Selecione uma opção', opcoes)
+
+# Exibir a opção selecionada
+st.write('Você selecionou:', selected_option)
