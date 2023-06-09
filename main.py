ca8386adf4c9bc40e04ceb23b975f46d6d795882
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-06-09 13:34:52 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import streamlit as st
 tickers = investpy.get_stocks_list(country='brazil')
 tickers = sorted(tickers)
 
-st.sidebar.header('Menu de Ações da B3')
+#st.sidebar.header('Menu de Ações da B3')
 
-ticker = st.sidebar.selectbox('Selecione uma ação: ', tickers)
+#ticker = st.sidebar.selectbox('Selecione uma ação: ', tickers)
 
-st.sidebar.write('Você selecionou ', ticker)
+#st.sidebar.write('Você selecionou ', ticker)
 
-st.header(ticker)
-st.write('Você selecionou ', ticker)
+#st.header(ticker)
+#st.write('Você selecionou ', ticker)
