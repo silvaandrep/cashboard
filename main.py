f48a252a2b409b8db76e9c4f751b76358dfa1215
silvaandrep <103048097+silvaandrep@users.noreply.github.com>
2023-06-09 13:07:26 -0300

    Update main.py
    c

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Título página')
 st.header('Header Principal')
-st.write('Você selecionou:', selected_option)
+st.write('Você selecionou')
 st.sidebar.header('Header Sidebar')
 st.sidebar.write('Write Sidebar')
 
